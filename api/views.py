53bbbd66ab9a205f6f5bb38fa495c9e7f9bf5328
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 20:22:59 +0530

    Create the API Layer – api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from backend.models import Blog
+from api.serializers import BlogSerializer
 
 # Create your views here.
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all().order_by('-created_at')
+    serializer_class = BlogSerializer
