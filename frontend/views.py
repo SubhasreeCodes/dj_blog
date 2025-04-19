8000963dd87770f51e6419623800f9fa757f31a5
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 20:30:24 +0530

    Frontend urls and views

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from backend.models import Blog
+
 
 # Create your views here.
+
+def home(request):
+    blogs = Blog.objects.all().order_by('-created_at')
+    return render(request, 'frontend/home.html', {'blogs': blogs})
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    return render(request, 'frontend/detail.html', {'blog': blog})
